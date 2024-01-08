96b2662816284e8cd0133127e9c04226185992a6
mistWil <146816285+mistWil@users.noreply.github.com>
2024-01-08 16:39:27 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,20 +8,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
-
-	if (parent == NULL)
-		return (NULL);
+	binary_tree_t *new_node;
 
 	new_node = binary_tree_node(parent, value);
 
+	if (new_node == NULL)
+		return (NULL);
+
 	if (parent->right == NULL)
 		parent->right = new_node;
+		
 	else if (parent->right != NULL)
 	{
+		new_node->parent = parent;
 		new_node->right = parent->right;
 		parent->right = new_node;
-		new_node->right->parent = new_node;
+		parent->right->parent = new_node;
 	}
 	return (new_node);
 }
