7b9a118f89a37efd4ef817199a052e6d5c9054f1
mistWil <7448@holbertonstudents.com>
2024-01-09 12:12:12 +0100

    add more betty changes

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);  /*Call the function for the current node*/
-	binary_tree_preorder(tree->left, func);    /*Recursively traverse the left subtree*/
-	binary_tree_preorder(tree->right, func);   /*Recursively traverse the right subtree*/
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
