fa94937d20b11fc28e8d1ab091832061129dc47f
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-09 11:59:07 -0800

    tree height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: the height of a binary tree.
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (height_left >= height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
+
+}
