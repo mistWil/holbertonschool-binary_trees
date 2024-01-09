ec419784fcad16a2ec7eb1cfc0e1fb54e0ea02a6
mistWil <7448@holbertonstudents.com>
2024-01-09 12:10:43 +0100

    add more betty changes

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal
- * @tree: Pointer to the root node of the tree to traverse
+ * binary_tree_preorder - Goes through a binary
+ * tree using pre-order traversal
+ * @tree: Pointer to the root node of the
+ * tree to traverse
  * @func: Pointer to the function to call for each node
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
