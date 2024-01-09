40cf99fd1a9568d052f3c5b0ef930c306e7c78f5
mistWil <7448@holbertonstudents.com>
2024-01-09 10:58:28 +0100

    add betty-style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,29 +1,39 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
- * @parent: pointer to the parent node.
- * @value: integer stored in the node.
- * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);
 
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->right == NULL)
-		parent->right = new_node;
-		
-	else if (parent->right != NULL)
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+
+	if (parent->right != NULL)
 	{
-		new_node->parent = parent;
 		new_node->right = parent->right;
-		parent->right = new_node;
 		parent->right->parent = new_node;
 	}
+	else
+	{
+		new_node->right = NULL;
+	}
+
+	parent->right = new_node;
+
 	return (new_node);
 }
+
