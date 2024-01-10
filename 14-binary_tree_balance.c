ef6b50c7cc7697631193f1f560d27260d0f3817f
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-10 10:56:32 -0800

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,8 +30,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	height_left = binary_tree_balance(tree->left);
-	height_right = binary_tree_balance(tree->right);
+	height_left = height(tree->left);
+	height_right = height(tree->right);
 	return (height_left - height_right);
 }
