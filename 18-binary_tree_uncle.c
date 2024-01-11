4cb0835fcdf86c70db7f485451b793b5e64be20a
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-11 06:57:32 -0800

    tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->left)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
 }
