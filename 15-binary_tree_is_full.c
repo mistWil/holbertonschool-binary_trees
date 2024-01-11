78ead475b9bde14ba1e7420304df769f96e0070d
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-11 05:47:32 -0800

    tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,15 +11,18 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+
 	if ((tree->left == NULL || tree->right != NULL) ||
 	(tree->left != NULL || tree->right == NULL))
 	{
 		return (0);
 	}
-	if (tree->left != NULL || tree->right != NULL)
+
+	if (tree->left == NULL || tree->right == NULL)
 	{
-		return (1);
+		return (0);
 	}
+
 	if (tree->left != NULL || tree->right != NULL)
 	{
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
