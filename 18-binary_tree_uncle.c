6df501c9f868998e8804106c1d12fefdd1054e44
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-11 06:42:04 -0800

    tree uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - function to check if there is an uncle
+ * @node: is a pointer to the node to find the uncle
+ * Return: a pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+	{
+		return (node->parent->parent->right);
+	}
+
+	else
+	{
+		return (node->parent->parent->left);
+	}
+}
