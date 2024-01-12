275e8bc8662c21ed2ef5a133d92a626593075527
mistWil <7448@holbertonstudents.com>
2024-01-12 10:53:44 +0100

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,11 +11,14 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+	parent = node->parent;
+	grandparent = parent->parent;
 
 	if (grandparent->left == parent)
 		return (grandparent->right);
